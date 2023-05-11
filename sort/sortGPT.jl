cab54933da6ca09ee02e6c287e99a5f49e1bd420
HopelessSoap <66231495+HopelessSoap@users.noreply.github.com>
2023-05-10 17:31:00 -0700

    Add files via upload

--- sort/sortGPT.jl
+++ sort/sortGPT.jl
@@ -1,3 +1,59 @@
-# I asked chatGPT to create a sorting algorithm written in Julia. It made this
+# I asked chatGPT to create some sorting algorithms. Here they are
 
-#using Random; x = shuffle(collect(1:8)); println(x); println(recursive_pairwise_bogo_sort(x))
+#= The Recursive Bitonic Shell Sort algorithm is a sorting algorithm that combines three well-known sorting algorithms: Shell Sort, Bitonic Sort,
+and Recursive Sort. The algorithm uses a combination of these three algorithms to sort an array of elements in ascending or descending order. - ChatGPT =#
+function bitonic_shell_sort!(A)
+    function bitonic_sort!(A, start, stop, direction, pass)
+        if stop - start <= 1
+            return
+        end
+        mid = div(start + stop, 2)
+        if pass >= 5
+            bitonic_sort!(A, start, mid, !direction, pass - 1)
+            bitonic_sort!(A, mid + 1, stop, direction, pass - 1)
+            bitonic_merge!(A, start, stop, direction)
+        else
+            if direction == (A[start] > A[stop])
+                A[start], A[stop] = A[stop], A[start]
+            end
+        end
+    end
+
+    function bitonic_merge!(A, start, stop, direction)
+        if stop - start <= 1
+            return
+        end
+        mid = div(start + stop, 2)
+        for i in start:mid - 1
+            if (A[i] > A[mid + i - start]) == direction
+                A[i], A[mid + i - start] = A[mid + i - start], A[i]
+            end
+            update!(A,i,mid+i-start,i)
+        end
+        bitonic_merge!(A, start, mid, direction)
+        bitonic_merge!(A, mid + 1, stop, direction)
+    end
+
+    n = length(A)
+    gap = div(n, 2)
+    while gap > 0
+        for i = gap + 1:n
+            j = i
+            while j > gap && A[j - gap] > A[j]
+                A[j - gap], A[j] = A[j], A[j - gap]
+                update!(A,j,j-gap,j)
+                j -= gap
+            end
+        end
+        pass = log2(gap)
+        bitonic_sort!(A, 1, n, true, pass)
+        gap = div(gap, 2)
+    end
+    OA[] = A
+    col[] = A
+    return A
+end
+
+
+
+#using Random; x = shuffle(collect(1:32)); println(x); println(GPT_sort!(x))
