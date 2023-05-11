573533b8acdb7d0822ee96ed35d2541c4aa5ee95
HopelessSoap <66231495+HopelessSoap@users.noreply.github.com>
2023-05-11 16:34:16 -0700

    Add files via upload

--- sort/main.jl
+++ sort/main.jl
@@ -1,6 +1,6 @@
 using Random, Observables, GLMakie
 include("sortBubble.jl"); include("sortQuick.jl"); include("sortHeap.jl"); include("sortGnome.jl"); include("sortShell.jl"); include("sortMerge.jl")
-include("sortStooge.jl"); include("sortBitonic.jl"); include("sortGPT.jl")
+include("sortStooge.jl"); include("sortBitonic.jl")
 GLMakie.activate!(
     title = "Sort", 
     fxaa = false, 
@@ -10,7 +10,7 @@ GLMakie.activate!(
     focus_on_show = false
 )
 
-arrLength = 16; println("Array length = $arrLength") # length of the array
+arrLength = 512; println("Array length = $arrLength") # length of the array
 arrType = "shuffle"; println("Array type = $shuffle") # type of array
 sleepTime = 0; println("Sleep time = $sleepTime seconds ") # controls speed of sort
 frequency = 0 # how often to update the plot; changes depending on the length of the plot
@@ -19,12 +19,11 @@ col = Observable(A)
 OA = Observable(A)
 
 # create visualization window
-# makie is slow as hell and i probably shouldn't be using it
 function viz(OA = OA, col = col, len = arrLength, A = A)
     f = Figure(backgroundcolor = :black)
     ax = Axis(f[1, 1], backgroundcolor = :black); hidedecorations!(ax); hidespines!(ax); tightlimits!(ax)
     x = 1:len
-    barplot!(x, OA, gap = 0, color = col, colormap = :viridis, colorrange = (1,length(A)), lowclip = :red); f
+    barplot!(x, OA, gap = 0, color = col, colormap = :plasma, colorrange = (1,length(A)), lowclip = :red); f
 end
 
 # update visualization
@@ -37,19 +36,14 @@ function update!(A, i, j, count)
 end
 
 # create and/or update the array
-# i don't think it's best practice to be using global like this. too bad
-function create(arrLength = arrLength)
-    # define array type
-    if arrType == "shuffle"; arr = shuffle(collect(1:arrLength))
-    elseif arrType == "reverse"; arr = reverse(collect(1:arrLength))
-    elseif arrType == "random"; arr = rand(1:arrLength, arrLength); end
-
-    # define update frequency based on length of plot
-    if arrLength < 256; global frequency = 1
-    elseif arrLength ≤ 512; global frequency = 2
-    elseif arrLength ≤ 1024; global frequency = 3
-    else; global frequency = 4; end
-    global A = arr
+function create(arrLength=arrLength, arrType=arrType)
+    arrFunctions = Dict(
+        "shuffle" => () -> shuffle(collect(1:arrLength)),
+        "reverse" => () -> reverse(collect(1:arrLength)),
+        "random" => () -> rand(1:arrLength, arrLength)
+    )
+    global A = arrFunctions[arrType]()
+    global frequency = arrLength < 256 ? 1 : arrLength ≤ 512 ? 2 : arrLength ≤ 1024 ? 3 : 4
     global OA = Observable(A)
     global col = Observable(A)
     viz()
